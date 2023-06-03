6f442331f244f4c0d48cf0cd1814b461e0c85dbd
Inukai <24tsuyomi@gmail.com>
2023-06-03 18:58:20 +0900

    QR関係の追加
    QR関係の追加
    多少、try catchの中が冗長なんで注意

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/planner/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/planner/YourService.java
@@ -1,69 +1,63 @@
 package jp.jaxa.iss.kibo.rpc.planner;
 
+import android.util.Log;
+
+import gov.nasa.arc.astrobee.PendingResult;
+import gov.nasa.arc.astrobee.internal.BaseRobot;
+import gov.nasa.arc.astrobee.internal.BaseRobotImpl;
+import gov.nasa.arc.astrobee.internal.CommandBuilder;
+import gov.nasa.arc.astrobee.internal.Publishable;
+import gov.nasa.arc.astrobee.types.FlightMode;
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.Kinematics;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 import java.util.List;
 
-import gov.nasa.arc.astrobee.types.Point;
-import gov.nasa.arc.astrobee.types.Quaternion;
-
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.CvType;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Rect;
+import org.opencv.objdetect.QRCodeDetector;
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
 
 public class YourService extends KiboRpcService {
+    private final String TAG = this.getClass().getSimpleName();
+
     @Override
     protected void runPlan1(){
+        int loop_counter = 0;
+        int target_id;
+
         // the mission starts
         api.startMission();
-        int loop_counter = 0;
 
-        while (true){
-            // get the list of active target id
-            List<Integer> list = api.getActiveTargets();
+        /*
+        * Move Point7
+        * */
+        MoveToWaypoint(waypoints_config.wp1);
+        MoveToWaypoint(waypoints_config.point7);
 
-            // move to a point
-            Point point = new Point(10.4d, -10.1d, 4.47d);
-            Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
-            api.moveTo(point, quaternion, false);
+        // Savefig
+        Mat image = new Mat();
+        image = api.getMatNavCam();
+        api.saveMatImage(image,"point7_nav.png");
 
-            // get a camera image
-            Mat image = api.getMatNavCam();
-
-            // irradiate the laser
-            api.laserControl(true);
-
-            // take active target snapshots
-            int target_id = 1;
-            api.takeTargetSnapshot(target_id);
-
-            /* ************************************************ */
-            /* write your own code and repair the ammonia leak! */
-            /* ************************************************ */
-
-            // get remaining active time and mission time
-            List<Long> timeRemaining = api.getTimeRemaining();
-
-            // check the remaining milliseconds of mission time
-            if (timeRemaining.get(1) < 60000){
-                break;
-            }
-
-            loop_counter++;
-            if (loop_counter == 2){
-                break;
-            }
-        }
         // turn on the front flash light
         api.flashlightControlFront(0.05f);
-        
-        // get QR code content
-        String mQrContent = yourMethod();
 
         // turn off the front flash light
         api.flashlightControlFront(0.00f);
+
+        // get QR code content
+        String mQrContent = read_QRcode(image);
+
 
         // notify that astrobee is heading to the goal
         api.notifyGoingToGoal();
@@ -76,18 +70,85 @@ public class YourService extends KiboRpcService {
         api.reportMissionCompletion(mQrContent);
     }
 
-    @Override
-    protected void runPlan2(){
-       // write your plan 2 here
-    }
+    private void MoveToWaypoint(Waypoint name){
 
-    @Override
-    protected void runPlan3(){
-        // write your plan 3 here
+        final int LOOP_MAX = 10;
+
+        int count = 0;
+        while(count < LOOP_MAX){
+            final Point point = new Point(
+                    (float)(name.posX + name.avoidX*count),
+                    (float)(name.posY + name.avoidY*count),
+                    (float)(name.posZ + name.avoidZ*count)  );
+            final Quaternion quaternion = new Quaternion(
+                    (float)name.quaX,
+                    (float)name.quaY,
+                    (float)name.quaZ,
+                    (float)name.quaW    );
+
+            Result result = api.moveTo(point, quaternion, true);
+            ++count;
+
+            if(result.hasSucceeded()){
+                break;
+            }
+            Log.w(TAG, "move Failure, retry");
+        }
     }
 
     // You can add your method
-    private String yourMethod(){
-        return "your method";
+    private String read_QRcode(Mat image){
+        String QRcode_content = "";
+        try{
+            api.saveMatImage(image,"QR.png");
+//        Mat mini_image = new Mat(image, new Rect(320, 240, 640, 480));
+            Mat mini_image = new Mat(image, new Rect(480, 360, 320, 240));
+            api.saveMatImage(mini_image,"QR_mini.png");
+
+            MatOfPoint2f points = new MatOfPoint2f();
+            Mat straight_qrcode = new Mat();
+            QRCodeDetector qrc_detector = new QRCodeDetector();
+            Boolean detect_success = qrc_detector.detect(mini_image, points);
+            Log.i(TAG,"detect_success is " + detect_success.toString());
+
+            QRcode_content = qrc_detector.detectAndDecode(mini_image, points, straight_qrcode);
+            Mat straight_qrcode_gray = new Mat();
+            straight_qrcode.convertTo(straight_qrcode_gray, CvType.CV_8UC1);
+            api.saveMatImage(straight_qrcode_gray,"QR_binary.png");
+            Log.i(TAG,"QRCode_content is " + QRcode_content);
+
+        } catch(Exception e){
+            ;
+        }
+
+        /**
+         * QRCode_CONTENT to REPORT_MESSEGE
+         */
+        switch(QRcode_content){
+            case "JEM":
+                QRcode_content = "STAY_AT_JEM";
+                break;
+            case "COLUMBUS":
+                QRcode_content = "GO_TO_COLUMBUS";
+                break;
+            case "RACK1":
+                QRcode_content = "CHECK_RACK_1";
+                break;
+            case "ASTROBEE":
+                QRcode_content = "I_AM_HERE";
+                break;
+            case "INTBALL":
+                QRcode_content = "LOOKING_FORWARD_TO_SEE_YOU";
+                break;
+            case "BLACK":
+                QRcode_content = "NO_PROBLEM";
+                break;
+            default:
+                QRcode_content = "";
+                break;
+        }
+
+        return QRcode_content;
+
     }
 }
---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/planner/waypoints_config.java
@@ -0,0 +1,59 @@
+package jp.jaxa.iss.kibo.rpc.planner;
+
+public interface waypoints_config {
+
+    // set Waypoint value
+// Write Position and Quaternion here.
+// Waypoint(pos_x, pos_y, pos_z, qua_x, qua_y, qua_z, qua_w, avoidX, avoidY, avoidZ)
+//X : right,left Y : back, front Z : down,up
+
+    //start -> (wp1)
+    //goal -> (point4, wp3)
+
+
+    // point1 -> ( point5, point6, point7, wp2, wp3)
+    Waypoint point1 = new Waypoint(11.318, -9.8, 5.251,
+            0.707, -0.707, 0, 0,
+            0, 0, 0);
+    // point2 -> (wp1, wp2)
+    Waypoint point2 = new Waypoint(10.625, -9.144, 4.6,
+            0, 0.707, 0, 0.707,
+            0, 0, 0);    // Point
+    // point3 -> (wp2, wp3)
+    Waypoint point3 = new Waypoint(10.722, -7.769, 4.6,
+            0, 0.707, 0, 0.707,
+            0, 0, 0);    // Point
+    // point4 -> (point5, wp2, wp3, goal)
+    Waypoint point4 = new Waypoint(10.6, -6.616, 5.205,
+            0, 0, 1, 0,
+            0, 0, 0);    // Point
+    // point5 -> (point1, point4, wp2, wp3)
+    Waypoint point5 = new Waypoint(10.992, -8.084, 5.34,
+            0, -0.707, 0, 0.707,
+            0, 0, 0);    // Point
+    // point6 -> (point1, point7, wp1, wp2)
+    Waypoint point6 = new Waypoint(11.21, -9.045, 4.941,
+            0, 0, 0, 1,
+            0, 0, 0);    // Point
+    // point7 -> (point1, point6, wp2)
+    Waypoint point7 = new Waypoint(11.362, -8.615, 4.883,
+            -0.5, 0.5, 0.5, 0.5,
+            0, 0, 0);    // QRcode
+
+    Waypoint goal_point = new Waypoint(11.143, -6.7607, 4.9654,
+            0, 0, -0.707, 0.707,
+            0, 0, 0);    // Point
+
+    // wp1 -> (point2, point6, point7, wp1, wp3, start)
+    Waypoint wp1 = new Waypoint(10.508, -9.769, 4.567,
+            0, 0, 0, 1,
+            0, 0, 0);    // way point
+    // wp2 -> (point1, point2, point3, point4, point5, point6, point7, wp1, wp3)
+    Waypoint wp2 = new Waypoint(10.571, -8.708, 5.251,
+            0, 0, -0.707, 0.707,
+            0, 0, 0.0);    // way point
+    //wp3 -> (point3, point4, point5, wp2, wp3, goal)
+    Waypoint wp3 = new Waypoint(10.571, -7.463, 5.326,
+            0, 0, -0.707, 0.707,
+            0, 0, 0.0);    // way point
+}
