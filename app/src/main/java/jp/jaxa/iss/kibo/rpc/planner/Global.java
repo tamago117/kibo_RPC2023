ae4f8db8164fa682abf27134ee6520fc41d89584
oshima <66927117+nupenupe@users.noreply.github.com>
2023-06-24 14:38:55 +0900

    残り時間修正

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/planner/Global.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/planner/Global.java
@@ -7,7 +7,7 @@ public class Global {
     //0.5は30秒Over
     //0.75は10秒~1分ぐらい
     //1.0は50％ぐらい
-    public static double RemainingTime = 1.25*60*1000; //残り時間
+    public static double RemainingTime = 1.0*60*1000; //残り時間
     public static double PhaseRemaintime = 0;
     public static boolean Finflag = false;
 }
